aa15a1d4fc1d54612a344519f245cbdb77260862
Stephen Studer <stephenkstuder@gmail.com>
2020-12-10 12:00:22 -0700

    setup simple build script

---
+++ src/index.js
@@ -0,0 +1,3 @@
+console.log("Hello From Webpack");
+console.log("Hello From Webpack");
+console.log("Hello From Webpack");
---
+++ webpack.config.js
@@ -0,0 +1,4 @@
+const { ModuleFilenameHelpers } = require("webpack");
+module.exports = {
+    entry: "./src/index.js"
+};

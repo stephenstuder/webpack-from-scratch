acf53ae01bf07153aa2014e5f21bc9c43e52fdd3
Stephen Studer <stephenkstuder@gmail.com>
2020-12-10 18:38:53 -0700

    scaffolding out of the module rules for babel, image compression and css compilation

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,20 @@ module.exports = {
         filename: "bundle.js",
         path: path.join(__dirname, "/build")
     }
+    // module: {
+    //     rules: [
+    //         {
+    //             test: /\.js$/,
+    //             use: "babel-loader"
+    //         },
+    //         {
+    //             test: /\.css$/,
+    //             use: ["style-loader", "css-loader"]
+    //         },
+    //         {
+    //             test: /\.jpeg$/,
+    //             use: ["url-loader"]
+    //         }
+    //     ]
+    // }
 };
